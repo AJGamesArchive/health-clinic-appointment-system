51ba244d67bd4ee050f4d62fe62827f3fa1b7c1d
jess-excell <jessexcell@outlook.com>
2025-05-13 20:27:25 +0100

    Web: Add basic universal header and footer

--- web/src/components/ui/Footer.tsx
+++ web/src/components/ui/Footer.tsx
@@ -1,6 +1,19 @@
+import buildNum from "../../static/BuildNumber"
+
 const Footer = () =>
-    <div style={{width: "100%", backgroundColor: "red"}}>
-        <p>FOOTER AAAA</p>
+    <div className="footer">
+        <h5>HCAMS</h5>
+        <h5>Health Clinic Appointment Management System</h5>
+        <p>Prototype Health Clinic Appointment Management System developed for the Canterbury Christ Church University Advanced Databases and Big Data Assessment 2 Group Project (module code U14440).</p>
+        <h4>Contact Support</h4>
+        <p>Developers' contact details are as follows:</p>
+        <ul style={{paddingLeft: "18px"}}>
+            <li>Jessica Excell: <a href="mailto:je398@canterbury.ac.uk" style={{color: "white"}}>je398@canterbury.ac.uk</a></li>
+            <li>Ethan McGuinness: <a href="mailto:em814@canterbury.ac.uk" style={{color: "white"}}>em814@canterbury.ac.uk</a></li>
+            <li>Alfie Skinner: <a href="mailto:as2679@canterbury.ac.uk" style={{color: "white"}}>as2679@canterbury.ac.uk</a></li>
+            <li>Alex Ward: <a href="mailto:aw949@canterbury.ac.uk" style={{color: "white"}}>aw949@canterbury.ac.uk</a></li>
+        </ul>
+        <p style={{textAlign: "center"}}>2025 Health Clinic Appointment Management System (version {buildNum}).<br/>Do not redistribute.</p>
     </div>
 
 
--- web/src/components/ui/Header.tsx
+++ web/src/components/ui/Header.tsx
@@ -1,15 +1,14 @@
 import { Button } from "react-bootstrap"
-import "../../App.css"
 const Header = () =>
     <div className="header">
         <div className="two-content-container">
-            <div className="individual-content">
+            <div className="first" style={{ width: "30%" }}>
                 <h4 style={{ padding: "auto"}}>Health Clinic Appointment Management System</h4>
             </div>
-            <div className="individual-content" style={{ textAlign:"right" }}>
-                <Button variant="outline-secondary">Patients??? but this should change</Button>
-                <Button variant="outline-secondary" style={{ marginLeft: "10px" }}>Appointments</Button>
-                <Button variant="secondary" style={{ marginLeft: "10px" }}>Logout</Button>
+            <div className="second">
+                <Button variant="outline-secondary">Temp button 1</Button>
+                <Button variant="outline-secondary" style={{ margin: "5px" }}>Temp button 2</Button>
+                <Button variant="secondary">Login/logout button</Button>
             </div>
         </div>
     </div>
