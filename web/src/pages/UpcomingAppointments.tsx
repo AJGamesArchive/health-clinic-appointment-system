abeb1319ff63ff7f3f1140bf9ff39bc1b3e4e983
jess-excell <jessexcell@outlook.com>
2025-05-14 21:46:14 +0100

    Web: Update UpdateAppointmentModal to use hooks and (hopefully) be easier to complete when backend is made

--- web/src/pages/UpcomingAppointments.tsx
+++ web/src/pages/UpcomingAppointments.tsx
@@ -15,58 +15,96 @@ const UpcomingAppointments: React.FC = () => {
         doctorId: string,
         doctorName: string
     };
+
+    interface DetailAppointmentData {            
+        appointmentId: string,
+        date: string,
+        time: string,
+        doctorId: string,
+        doctorName: string,
+        patientId: string,
+        patientName: string,
+        createdAt: string,
+        updatedAt: string,
+
+        vitals: {
+            height: string,
+            weight: string,
+            temperature: string,
+            heartRate: string,
+            bloodPressure: string,
+        },
+        preAppointmentNotes: string,
+        postAppointmentNotes: string,
+        actionsTaken: string,
+    };
     
     const tempAppointmentData: AppointmentData[] = [
         { 
             appointmentId: "0",
             date: "2025-05-12",
-            time: "10.00 AM",
+            time: "10:00",
             doctorId: "1",
             doctorName: "Dr Jane Smith"
         },
         { 
             appointmentId: "1",
             date: "2025-05-12",
-            time: "12.00 PM",
+            time: "12:00",
             doctorId: "2",
             doctorName: "Dr John Smith"
         },
         { 
             appointmentId: "2",
             date: "2025-05-13",
-            time: "10.00 AM",
+            time: "10:00",
             doctorId: "1",
             doctorName: "Dr Jane Smith"
         },
     ];
-    const [ selectedAppointment, setSelectedAppointment ] = useState<AppointmentData>(tempAppointmentData[0]);
+    const [ selectedAppointment, setSelectedAppointment ] = useState<DetailAppointmentData | undefined>(undefined);
 
-    const tempAppointmentCards = tempAppointmentData.map((appointment) => {
-        return (
-            <Card key={appointment.appointmentId} onClick={() => setUpdateVisible(true)}>
-                <h5>Appointment with {appointment.doctorName}</h5>
-                <p style={{margin: 0}}>Date: {appointment.date}</p>
-                <p style={{margin: 0}}>Time: {appointment.time}</p>
-            </Card>
-        );
-    });
-
-    // const tempAppointmentCards = null
-    // Uncomment this to see view with no appointments
-
-    function handleUpdateVisible(id: string) {
-        const selectedAppointment = tempAppointmentData.find(x => x.appointmentId === id);
-        if (!selectedAppointment) {
-            return;
-        };
-        setSelectedAppointment(selectedAppointment);
+    function handleUpdateVisible(appointment : AppointmentData) {
+        // Get actual data
+        
+        // Add extra data by hand for time being (normally would use get on appointment ID to 
+        // get full appointment data)
+        const fullData: DetailAppointmentData = {...appointment, 
+            patientId: "1",
+            patientName: "Jane Smith",
+            createdAt: "2025-05-12", 
+            updatedAt: "2025-05-13",
+            vitals: {
+                bloodPressure: "",
+                height: "",
+                weight: "",
+                heartRate: "",
+                temperature: ""
+            },
+            preAppointmentNotes: "Patient is dying",
+            postAppointmentNotes: "Patient is dead",
+            actionsTaken: "N/A"
+        }
+        setSelectedAppointment(fullData);
         setUpdateVisible(true);
-    }
+    };
+
+    const tempAppointmentCards = tempAppointmentData.map((appointment) => (
+        <Card key={appointment.appointmentId} onClick={() => handleUpdateVisible(appointment)}>
+            <h5>Appointment with {appointment.doctorName}</h5>
+            <p style={{margin: 0}}>Date: {appointment.date}</p>
+            <p style={{margin: 0}}>Time: {appointment.time}</p>
+        </Card>
+    ));
+
+    // Uncomment this to see view with no appointments    
+    // const tempAppointmentCards = null
 
     return (
         <Layout>
             <CreateAppointmentModal visible={createVisible} setVisible={setCreateVisible}/>
-            <UpdateAppointmentModal visible={updateVisible} setVisible={setUpdateVisible} appointment={{...selectedAppointment, patientId: "1", patientName: "Test"}}/>
+            { selectedAppointment && <UpdateAppointmentModal visible={updateVisible} setVisible={setUpdateVisible} selectedAppointment={selectedAppointment}/>}
+            
             <h2>Upcoming Appointments <Button variant="primary" onClick={() => setCreateVisible(true)}>Book Appointment</Button></h2>            
             { tempAppointmentCards ? 
                 tempAppointmentCards 
