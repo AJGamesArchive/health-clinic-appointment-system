95861b0b5d064ac466daa43cdfcf8c1b1d5e310f
Ethan McGuinness <EM814@canterbury.ac.uk>
2025-05-15 23:17:48 +0100

    Updated AddLifeStyle pop up box

--- web/src/components/tabs/addLiftStyleEntry.tsx
+++ web/src/components/tabs/addLiftStyleEntry.tsx
@@ -24,23 +24,211 @@ const AddEntry: React.FC<AddEntryProps> = ({ onClose }) => {
     const renderEntryForm = () => {
         switch (selectedType) {
             case "Smoking Status":
-                return <div>Smoking Status Form</div>;
+                return(
+                
+                <div className="smoking-status">
+                <input
+                 type="text" 
+                 name="smokingStatus" 
+                 placeholder='Smoking Status'
+                 />
+                <input
+                  type="date" 
+                  name="statusAsOf" 
+                  placeholder='Status As Of'
+                />
+                <input
+                  type="text" 
+                  name="notes"
+                  placeholder='Notes'
+                />
+                </div>    
+                ); 
             case "Alcohol Consumption":
-                return <div>Alcohol Consumption Form</div>;
+                return (
+                <div className="alcohol-consumption">
+                <input
+                 type="text" 
+                 name="alcoholConsumption" 
+                 placeholder='Alcohol Consumption'
+                 />
+                <input
+                  type="date" 
+                  name="consumptionAsOf" 
+                  placeholder='Consumption As Of'
+                />
+                <input
+                  type="text" 
+                  name="notes"
+                  placeholder='Notes'
+                  />
+                </div>
+                );
             case "Recreational Drug Usage":
-                return <div>Recreational Drug Usage Form</div>;
+                return (
+                <div className="recreational-drug-usage">
+                <input
+                 type="text" 
+                 name="recreationalDrugUsage"
+                  placeholder='Recreational Drug Usage'
+                />
+                <input
+                  type="text" 
+                  name="usage" 
+                  placeholder='Usage'
+                />
+                <input
+                  type="date" 
+                  name="useAsOf" 
+                  placeholder='Use As Of'
+                />
+                <input
+                  type="text" 
+                  name="notes"
+                  placeholder='Notes'
+                />
+                </div>
+                );
             case "Exercise Frequency":
-                return <div>Exercise Frequency Form</div>;
+                return (
+                <div className="exercise-frequency">
+                <input
+                 type="text" 
+                 name="exerciseFrequency" 
+                 placeholder='Exercise Frequency'
+                />
+                <input
+                  type="text" 
+                  name="frequency" 
+                  placeholder='Frequency'
+                />
+                <input
+                  type="date" 
+                  name="frequencyAsOf" 
+                  placeholder='Frequency As Of'
+                />
+                <input
+                  type="text" 
+                  name="notes"
+                  placeholder='Notes'
+                />
+                </div>
+                )
             case "Sleep Quality":
-                return <div>Sleep Quality Form</div>;
+                return (
+                <div className="sleep-quality">
+                <input
+                 type="text" 
+                 name="sleepQuality" 
+                 placeholder='Sleep Quality'
+                />
+                <input
+                  type="date" 
+                  name="qualityAsOf" 
+                  placeholder='Quality As Of'
+                />
+                <input
+                  type="text" 
+                  name="notes"
+                  placeholder='Notes'
+                />
+                </div>
+                );
             case "Social Support":
-                return <div>Social Support Form</div>;
+                return (
+                <div className="social-support">
+                <input
+                 type="text" 
+                 name="socialSupport" 
+                 placeholder='Social Support'
+                />
+                <input
+                  type="date" 
+                  name="supportAsOf" 
+                  placeholder='Support As Of'
+                />
+                <input
+                  type="text" 
+                  name="notes"
+                  placeholder='Notes'
+                />
+                </div>
+                );
             case "Travel History":
-                return <div>Travel History Form</div>;
+                return (
+                <div className="travel-history">
+                <input
+                 type="text" 
+                 name="travelHistory" 
+                 placeholder='Travel History'
+                />
+                <input
+                  type="date" 
+                  name="date" 
+                  placeholder='Date'
+                />
+                <input
+                  type="text" 
+                  name="duration" 
+                  placeholder='Duration'
+                />
+                <input
+                  type="text" 
+                  name="notes"
+                  placeholder='Notes'
+                />
+                </div>
+                );
             case "Family Conditions":
-                return <div>Family Conditions Form</div>;
+                return (
+                <div className="family-conditions">
+                <input
+                 type="text" 
+                 name="familyConditions" 
+                 placeholder='Family Conditions'
+                />
+                <input
+                  type="text" 
+                  name="typicalCause" 
+                  placeholder='Typical Cause'
+                />
+                <input
+                  type="number" 
+                  name="typicalAgeOfDiagnosis" 
+                  placeholder='Typical Age Of Diagnosis'
+                />
+                <input
+                  type="text" 
+                  name="notes"
+                  placeholder='Notes'
+                />
+                </div>
+                );
             case "Environmental Factors":
-                return <div>Environmental Factors Form</div>;
+                return (
+                <div className="environmental-factors">
+                <input
+                 type="text" 
+                 name="environmentalFactors" 
+                 placeholder='Environmental Factors'
+                />
+                <input
+                  type="text" 
+                  name="exposure" 
+                  placeholder='Exposure'
+                />
+                <input
+                  type="date" 
+                  name="exposureAsOf" 
+                  placeholder='Exposure As Of'
+                />
+                <input
+                  type="text" 
+                  name="notes"
+                  placeholder='Notes'
+                />
+                </div>
+                );
             default:
                 return null;
         }
