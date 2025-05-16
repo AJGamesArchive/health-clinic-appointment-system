0121ad0ffc17c20d459e357c17ed2cd56ee9755a
Ethan McGuinness <EM814@canterbury.ac.uk>
2025-05-17 00:20:47 +0100

    updated style and components in life style pages

--- web/src/components/tabs/addLiftStyleEntry.tsx
+++ web/src/components/tabs/addLiftStyleEntry.tsx
@@ -27,151 +27,175 @@ const AddEntry: React.FC<AddEntryProps> = ({ onClose }) => {
                 return(
                 
                 <div className="smoking-status">
+                  <p>Smoking Status</p>
                 <input
                  type="text" 
                  name="smokingStatus" 
-                 placeholder='Smoking Status'
+                 placeholder='Enter Status'
                  />
+                 <p>date</p>
                 <input
                   type="date" 
                   name="statusAsOf" 
                   placeholder='Status As Of'
                 />
+                <p>notes</p>
                 <input
                   type="text" 
                   name="notes"
-                  placeholder='Notes'
+                  placeholder='Enter Notes'
                 />
                 </div>    
                 ); 
             case "Alcohol Consumption":
                 return (
                 <div className="alcohol-consumption">
+                  <p>Alcohol Consumption</p>
                 <input
                  type="text" 
                  name="alcoholConsumption" 
-                 placeholder='Alcohol Consumption'
+                 placeholder=' Enter Alcohol Consumption'
                  />
+                 <p>Date</p>
                 <input
                   type="date" 
                   name="consumptionAsOf" 
                   placeholder='Consumption As Of'
                 />
+                <p>notes</p>
                 <input
                   type="text" 
                   name="notes"
-                  placeholder='Notes'
+                  placeholder='Enter Notes'
                   />
                 </div>
                 );
             case "Recreational Drug Usage":
                 return (
                 <div className="recreational-drug-usage">
+                <p>Recreational Drug Usage</p>
                 <input
                  type="text" 
                  name="recreationalDrugUsage"
-                  placeholder='Recreational Drug Usage'
+                  placeholder='Add Recreational Drug Usage'
                 />
+                <p>Usage</p>
                 <input
                   type="text" 
                   name="usage" 
-                  placeholder='Usage'
+                  placeholder='Add Usage'
                 />
+                <p>Date</p>
                 <input
                   type="date" 
                   name="useAsOf" 
                   placeholder='Use As Of'
                 />
+                <p>notes</p>
                 <input
                   type="text" 
                   name="notes"
-                  placeholder='Notes'
+                  placeholder='Add Notes'
                 />
                 </div>
                 );
             case "Exercise Frequency":
                 return (
                 <div className="exercise-frequency">
+                <p>Exercise Frequency</p>
                 <input
                  type="text" 
                  name="exerciseFrequency" 
-                 placeholder='Exercise Frequency'
+                 placeholder='Add Exercise Frequency'
                 />
+                <p>Frequency</p>
                 <input
                   type="text" 
                   name="frequency" 
-                  placeholder='Frequency'
+                  placeholder='Add Frequency'
                 />
+                <p>Date</p>
                 <input
                   type="date" 
                   name="frequencyAsOf" 
                   placeholder='Frequency As Of'
                 />
+                <p>notes</p>
                 <input
                   type="text" 
                   name="notes"
-                  placeholder='Notes'
+                  placeholder='Add Notes'
                 />
                 </div>
                 )
             case "Sleep Quality":
                 return (
                 <div className="sleep-quality">
+                <p>Sleep Quality</p>
                 <input
                  type="text" 
                  name="sleepQuality" 
-                 placeholder='Sleep Quality'
+                 placeholder='Add Sleep Quality'
                 />
+                <p>Date</p>
                 <input
                   type="date" 
                   name="qualityAsOf" 
                   placeholder='Quality As Of'
                 />
+                <p>notes</p>
                 <input
                   type="text" 
                   name="notes"
-                  placeholder='Notes'
+                  placeholder='Add Notes'
                 />
                 </div>
                 );
             case "Social Support":
                 return (
                 <div className="social-support">
+                <p>Social Support</p>
                 <input
                  type="text" 
                  name="socialSupport" 
-                 placeholder='Social Support'
+                 placeholder='Add Social Support'
                 />
+                <p>Date</p>
                 <input
                   type="date" 
                   name="supportAsOf" 
                   placeholder='Support As Of'
                 />
+                <p>notes</p>
                 <input
                   type="text" 
                   name="notes"
-                  placeholder='Notes'
+                  placeholder='Add Notes'
                 />
                 </div>
                 );
             case "Travel History":
                 return (
                 <div className="travel-history">
+                <p>Travel History</p>
                 <input
                  type="text" 
                  name="travelHistory" 
                  placeholder='Travel History'
                 />
+                <p>Date</p>
                 <input
                   type="date" 
                   name="date" 
                   placeholder='Date'
                 />
+                <p>Duration</p>
                 <input
                   type="text" 
                   name="duration" 
                   placeholder='Duration'
                 />
+                <p>Notes</p>
                 <input
                   type="text" 
                   name="notes"
@@ -182,50 +206,58 @@ const AddEntry: React.FC<AddEntryProps> = ({ onClose }) => {
             case "Family Conditions":
                 return (
                 <div className="family-conditions">
+                <p>Family Conditions</p>
                 <input
                  type="text" 
                  name="familyConditions" 
                  placeholder='Family Conditions'
                 />
+                <p>Cause</p>
                 <input
                   type="text" 
                   name="typicalCause" 
                   placeholder='Typical Cause'
                 />
+                <p>Age of Diagnosis</p>
                 <input
                   type="number" 
                   name="typicalAgeOfDiagnosis" 
                   placeholder='Typical Age Of Diagnosis'
                 />
+                <p>Notes</p>
                 <input
                   type="text" 
                   name="notes"
-                  placeholder='Notes'
+                  placeholder='Add Notes'
                 />
                 </div>
                 );
             case "Environmental Factors":
                 return (
                 <div className="environmental-factors">
+                <p>Environmental Factors</p>
                 <input
                  type="text" 
                  name="environmentalFactors" 
-                 placeholder='Environmental Factors'
+                 placeholder='Add Environmental Factors'
                 />
+                <p>Exposure</p>
                 <input
                   type="text" 
                   name="exposure" 
-                  placeholder='Exposure'
+                  placeholder='Add Exposure'
                 />
+                <p>Date</p>
                 <input
                   type="date" 
                   name="exposureAsOf" 
                   placeholder='Exposure As Of'
                 />
+                <p>Notes</p>
                 <input
                   type="text" 
                   name="notes"
-                  placeholder='Notes'
+                  placeholder='Add Notes'
                 />
                 </div>
                 );
