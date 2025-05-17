d1aaf706e292719d39e5cf6a5acb29ef15519359
jess-excell <jessexcell@outlook.com>
2025-05-18 00:23:10 +0100

    Web: Update App.tsx to match appopriate patient, doctor and admin access permissions

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -43,28 +43,33 @@ const App: React.FC = () => {
         />}>
           <Route path="/profile" Component={Profile}/>
           <Route path="/home" element={<HomePage/>}/>
+          <Route path="/appointments" element={<UpcomingAppointments />}/>
+        </Route>
+        
+        {/* No patient-only pages (I think?) */}
+
+        <Route element={<ProtectedRoute
+          requiredRank="Doctor"
+          loading={<PageLoading/>}
+          fallback={<PageAccessDenied/>}
+        />}>
+          <Route path="/patients" element={<PatientList/>}/>
+          <Route path="/patient-profile" element={<PatientProfile />}/>
+        </Route>
+        <Route element={<ProtectedRoute
+          requiredRank="Admin"
+          loading={<PageLoading/>}
+          fallback={<PageAccessDenied/>}
+        />}>
           <Route path="/patient-profile" element={<PatientProfile />}/>
           <Route path="/doctor-profile" element={<DoctorProfile />}/>
           <Route path="/admin-profile" element={<AdminProfile />}/>
-          <Route path="/appointments" element={<UpcomingAppointments />}/>
           <Route path="/create-patient" element={<PatientCreation />}/>
           <Route path="/create-doctor" element={<DoctorCreation />}/>
           <Route path="/create-admin" element={<AdminCreation />}/>
           <Route path="/patients" element={<PatientList/>}/>
           <Route path="/doctors" element={<DoctorList />}/>
           <Route path="/admins" element={<AdminList />}/>
-        </Route>
-        {
-          //* Admin Only Routes
-        }
-        <Route element={<ProtectedRoute
-          requiredRank="Admin"
-          loading={<PageLoading/>}
-          fallback={<PageAccessDenied/>}
-        />}>
-          {
-            //TODO Add Admin Only Routes Here
-          }
         </Route>
       </Routes>
     </Suspense>
