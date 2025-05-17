e9eda65dc14b9590d11e79d723303abfb37b91b1
jess-excell <jessexcell@outlook.com>
2025-05-17 19:32:18 +0100

    Web: Add UseProfileHook to get all account data for a given account. Use Hook on AdminProfile, but yet to be used on other screens.
    Co-authored-by: Alex Ward <AJGamesArchive@users.noreply.github.com>

--- web/src/hooks/UseAdminProfile.ts
+++ web/src/hooks/UseAdminProfile.ts
@@ -1,52 +1,50 @@
-import { useState } from "react";
-import { useAuthContext } from "../contexts/AuthContext";
-import { UseRouteAuthHook } from "./security/UseRouteAuth";
+import { useEffect, useState } from "react";
+import { useAccountProfile, UseProfileHook } from "./UseProfileHook";
+import { cloneDeep } from "lodash";
+import AccountData from "../types/data/AccountData";
+import AdminData from "../types/data/AdminData";
 
-export function useAdminProfile() {
-    const auth: UseRouteAuthHook = useAuthContext();
+interface UseAdminProfileHook {
+    profile: UseProfileHook;
+    updateProfile: () => void;
+    toggleUpdateEnabled: () => void;
+    updateDisabled: boolean,
+    updateField: (field: keyof AccountData, newValue: string, adminField?: keyof AdminData) => void;
+    modifiedProfile: AccountData | undefined;
+};
 
-    var profile;
-    if (auth.user?.role === "Admin") {
-        profile = {
-            email: auth.user?.email,
-            title: auth.user?.title,
-            forename: auth.user?.forenames,
-            surname: auth.user?.surname,
-            role: auth.user?.role // This should actually use the embedded data
-        };
-    } else {
-        profile = {
-            email: "janesmith@gmail.com",
-            title: "Miss",
-            forename: "Jane",
-            surname: "Smith",
-            role: "CEO"
-        };
-    }
-    
-    const [ forename, updateForename ] = useState<string>(profile.forename);
-    const [ surname, updateSurname ] = useState<string>(profile.surname);
-    const [ email, updateEmail ] = useState<string>(profile.email);
-    const [ title, updateTitle ] = useState<string>(profile.title);
-    const [ role, updateRole ] = useState<string>(profile.role);
+export function useAdminProfile(): UseAdminProfileHook {
+    // States
+    const useProfile = useAccountProfile();
     const [ updateDisabled, setUpdateDisabled ] = useState<boolean>(true);
+    const [ modifiedProfile, setModifiedProfile ] = useState<AccountData | undefined>(undefined);
+
+    function updateField(field: keyof AccountData, newValue: string, adminField?: keyof AdminData) {
+        if (!modifiedProfile) return;
+        setModifiedProfile({...modifiedProfile, [field]: (!adminField) ? newValue : {
+            [adminField]: newValue
+        } });
+    };
+
+    useEffect(() => {
+        if(useProfile.data && !modifiedProfile) (setModifiedProfile(cloneDeep(useProfile.data)))
+    }, [ useProfile.data])
 
     function updateProfile () {
         // Send all the information to the backend to save it
-        console.log("Save profile button pressed")
-    }
+        console.log("Save profile button pressed");
+    };
 
     function toggleUpdateEnabled() {
         setUpdateDisabled(!updateDisabled);
-    }
+    };
 
     return ({
-        forename, updateForename,
-        surname, updateSurname,
-        email, updateEmail,
-        title, updateTitle,
-        role, updateRole,
-        updateDisabled: updateDisabled, toggleUpdateEnabled,
+        profile: useProfile,
         updateProfile,
+        toggleUpdateEnabled,
+        updateDisabled,
+        updateField,
+        modifiedProfile,
     });
 }
--- web/src/pages/AdminProfile.tsx
+++ web/src/pages/AdminProfile.tsx
@@ -2,21 +2,39 @@ import Layout from "../components/ui/Layout";
 import { Badge, Button  } from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
 import { useAdminProfile } from "../hooks/UseAdminProfile";
-
+import DebugBlock from "../components/utilities/DebugBlock";
 
 const AdminProfile: React.FC = () => {
     const adminProfile = useAdminProfile();
+
+    if(!adminProfile.profile.data) return (
+        <div>
+            {adminProfile.profile.loading && (
+                <div>
+                    <b><i>Loading data...</i></b>
+                </div>
+            )}
+            {adminProfile.profile.error && (
+                <div>
+                    Error occurred: {adminProfile.profile.error}
+                </div>
+            )}
+        </div>
+    );
 
     // There should be a check here to see if a patient or doctor has tried to 
     // access this page (or someone who is logged out)
     return (
         <Layout>
+            <DebugBlock>
+                {JSON.stringify(adminProfile.modifiedProfile, null, 2)}
+            </DebugBlock>
             <div className="two-content-container">
                 <div className="first" style={{padding: "20px", border: "1px solid lightgray", borderRadius: "5px", margin: "5px"}}>
-                <h2>{adminProfile.forename} {adminProfile.surname}<Badge style={{margin: "0 10px", }} bg="success">Admin</Badge></h2>
+                <h2>{adminProfile.profile.data?.forenames} {adminProfile.profile.data.surname}<Badge style={{margin: "0 10px", }} bg="success">Admin</Badge></h2>
                 <Form>
                     <Form.Label>Title</Form.Label>
-                    <Form.Select value={adminProfile.title} onChange={(e) => adminProfile.updateTitle(e.target.value)} disabled={adminProfile.updateDisabled}>
+                    <Form.Select value={adminProfile.modifiedProfile?.title} onChange={(e) => adminProfile.updateField("title", e.target.value)} disabled={adminProfile.updateDisabled}>
                         <option>select a title</option>
                         <option>Ms</option>
                         <option>Mr</option>
@@ -25,17 +43,13 @@ const AdminProfile: React.FC = () => {
                         <option>Miss</option>
                     </Form.Select>
                     <Form.Label>Forename</Form.Label>
-                    <Form.Control placeholder="forename" value={adminProfile.forename} onChange={(e) => adminProfile.updateForename(e.target.value)} disabled={adminProfile.updateDisabled}/>
+                    <Form.Control placeholder="forename" value={adminProfile.modifiedProfile?.forenames} onChange={(e) => adminProfile.updateField("forenames", e.target.value)} disabled={adminProfile.updateDisabled}/>
                     <Form.Label>Surname</Form.Label>
-                    <Form.Control placeholder="surname" value={adminProfile.surname} onChange={(e) => adminProfile.updateSurname(e.target.value)} disabled={adminProfile.updateDisabled}/>
+                    <Form.Control placeholder="surname" value={adminProfile.modifiedProfile?.surname} onChange={(e) => adminProfile.updateField("surname", e.target.value)} disabled={adminProfile.updateDisabled}/>
                     <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" value={adminProfile.email} onChange={(e) => adminProfile.updateEmail(e.target.value)} disabled={adminProfile.updateDisabled}/>
+                    <Form.Control type="email" placeholder="name@example.com" value={adminProfile.modifiedProfile?.email} onChange={(e) => adminProfile.updateField("email", e.target.value)} disabled={adminProfile.updateDisabled}/>
                     <Form.Label>Role</Form.Label>
-                    <Form.Select value={adminProfile.role} onChange={(e) => adminProfile.updateRole(e.target.value)} disabled={adminProfile.updateDisabled}>
-                        <option>select a role</option>
-                        <option>CEO</option>
-                        <option>Assistant to the regional manager</option>
-                    </Form.Select>
+                    <Form.Control value={adminProfile.modifiedProfile?.adminData?.staffRole} onChange={(e) => adminProfile.updateField("adminData", e.target.value, "staffRole")} disabled={adminProfile.updateDisabled}/>
 
                     <div style={{textAlign: "center"}}>
                         { ! adminProfile.updateDisabled ? 
