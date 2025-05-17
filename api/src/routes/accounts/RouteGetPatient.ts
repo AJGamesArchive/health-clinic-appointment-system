240056ed8195cac8dc637002b1db2d23b7b19389
HammerCyclone <alfieskinner@yahoo.com>
2025-05-17 17:34:52 +0100

    API: Created get paitent endpoint, need service script to complete.

---
+++ api/src/routes/accounts/RouteGetPatient.ts
@@ -0,0 +1,71 @@
+// Imports
+//Import fastify
+import { FastifyRequest, FastifyReply } from 'fastify';
+
+//Import interfaces from the schema file
+import {
+	GetPatientHeaders,
+	GetPatientParams,
+	GetPatientReply206,
+	GetPatientReply,
+} from '../../schema/accounts/SchemaGetPatient.js';
+
+//Service function (abstract parent class)
+import AccountService from '../../services/database/AccountService.js';
+
+//Account data (child object) will be used to contain the logged in user accounts account to do operations
+import AccountData, { JWTAccountData } from '../../types/data/AccountData.js';
+
+/**
+ * @summary Route to  account data for a patient by a given doctor id
+ * @route GET /auth/internal/doctor/patient/:id
+ * @HammerCyclone
+ */
+
+//Use interfaces to insert into headers and querystring (req = Request, rep = Reply)
+const routeGetPatient = async (
+	req: FastifyRequest<{
+		Headers: GetPatientHeaders;
+		Params: GetPatientParams;
+  }>,
+	rep: FastifyReply,
+): Promise<void> => {
+	// Type user object
+	const user = req.user as JWTAccountData;
+	const id = req.params.id
+
+	// Validate if logged in user is requesting their own information
+	let reject: boolean = false;
+	if (user.role !== 'Doctor') reject = true;
+
+	if(reject) {
+		rep.status(403).send({
+			error: 'FORBIDDEN',
+			message: 'You do not have permission to access this resource.',
+		} as GetPatientReply);
+		return;
+	};
+
+	// Fetch account data
+	let Patient: AccountData | number;
+	Patient = await AccountService.getOne(id)
+
+	// Ensure DB request succeeded
+	if(typeof Patient === 'number') {
+		rep.status(500).send({
+			error: 'INTERNAL_SERVER_ERROR',
+			message: `An error occurred while fetching Patient data: Err: ${Patient}`,
+		} as GetPatientReply);
+		return;
+	};
+
+	// Filter and return data
+	rep.status(206).send({
+		...Patient,
+		data: {
+			...Patient.patientData,
+		},
+	} as GetPatientReply206);
+};
+
+export default routeGetPatient;
---
+++ api/src/schema/accounts/SchemaGetPatient.ts
@@ -0,0 +1,440 @@
+import PatientData from "../../types/data/PatientData";
+import MedicalHistoryDetails from "../../types/data/MedicalHistory";
+
+//Make interfaces based on the objects defined earlier
+export interface GetPatientHeaders {
+  'content-type': string;
+  'origin': string;
+};
+export interface GetPatientParams {
+  id: string;
+};
+
+export interface GetPatientReply200 {
+  id: string;
+  title: string;
+  forenames: string;
+  surname: string;
+  email: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+  data: PatientData;
+  medicalHistory: MedicalHistoryDetails | undefined;
+};
+
+export interface GetPatientReply206 {
+  title: string;
+  forenames: string;
+  surname: string;
+  data: PatientData
+};
+
+export interface GetPatientReply {
+  error: string;
+  message: string;
+};
+
+/**
+ * @summary API Endpoint Schema
+ * @route GET /auth/internal/doctor/patient/:id
+ * @HammerCyclone
+ */
+const schemaGetPatient = {
+  headers: {
+    type: 'object',
+    properties: {
+      'content-type': { type: 'string', enum: ['application/json'] },
+      'origin': { type: 'string' },
+    },
+    required: ['origin'],
+  },
+  params: {
+    type: 'object',
+    properties: {
+      id: { type: 'string' },
+    },
+    required: ['id'],
+  },
+  response: {
+    200: {
+      type: 'object',
+      properties: {
+        id: { type: 'string' },
+        title: { type: 'string' },
+        forenames: { type: 'string' },
+        surname: { type: 'string' },
+        email: { type: 'string' },
+        role: { type: 'string', enum: ['Patient', 'Admin'] },
+        createdAt: { type: 'string', format: 'date-time' },
+        updatedAt: { type: 'string', format: 'date-time' },
+        data: {
+          type: 'object',
+          properties: {
+            gender: { type: 'string' },
+            dateOfBirth: { type: 'string', format: 'date' },
+            contactInfo: {
+              type: 'object',
+              properties: {
+                email: { type: 'string' },
+                phone: { type: 'string' },
+              },
+              required: ['email', 'phone'],
+            },
+            emergencyContact: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  name: { type: 'string' },
+                  phone: { type: 'string' },
+                  relationship: { type: 'string' },
+                },
+                required: ['name', 'phone', 'relationship'],
+              },
+            },
+            address: {
+              type: 'object',
+              properties: {
+                addressLine1: { type: 'string' },
+                addressLine2: { type: 'string' },
+                city: { type: 'string' },
+                county: { type: 'string' },
+                postCode: { type: 'string' },
+              },
+              required: ['addressLine1', 'city', 'county', 'postCode'],
+            },
+            upcomingAppointments: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  appointmentId: { type: 'string' },
+                  date: { type: 'string' },
+                  time: { type: 'string' },
+                  doctorId: { type: 'string' },
+                  doctorName: { type: 'string' },
+                },
+                required: ['appointmentId', 'date', 'time', 'doctorId', 'doctorName'],
+              },
+            },
+            medicalInformation: {
+              type: 'object',
+              properties: {
+                bloodType: { type: 'string' },
+                sexAtBirth: { type: 'string' },
+                conditions: {
+                  type: 'array',
+                  items: { type: 'string' },
+                },
+                allergies: {
+                  type: 'array',
+                  items: { type: 'string' },
+                },
+              },
+              required: ['bloodType', 'sexAtBirth', 'conditions', 'allergies'],
+            },
+            lifeStyleHistory: {
+              type: 'object',
+              properties: {
+                smokingStatus: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      status: { type: 'string' },
+                      statusAsOf: { type: 'string', format: 'date' },
+                      notes: { type: 'string' },
+                    },
+                    required: ['status', 'statusAsOf', 'notes'],
+                  },
+                },
+                alcoholConsumption: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      consumption: { type: 'string' },
+                      consumptionAsOf: { type: 'string', format: 'date' },
+                      notes: { type: 'string' },
+                    },
+                    required: ['consumption', 'consumptionAsOf', 'notes'],
+                  },
+                },
+                recreationalDrugUse: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      drug: { type: 'string' },
+                      usage: { type: 'string' },
+                      useAsOf: { type: 'string', format: 'date' },
+                      notes: { type: 'string' },
+                    },
+                    required: ['drug', 'usage', 'useAsOf', 'notes'],
+                  },
+                },
+                exerciseFrequency: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      exercise: { type: 'string' },
+                      frequency: { type: 'string' },
+                      frequencyAsOf: { type: 'string', format: 'date' },
+                      notes: { type: 'string' },
+                    },
+                    required: ['exercise', 'frequency', 'frequencyAsOf', 'notes'],
+                  },
+                },
+                sleepQuality: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      quality: { type: 'string' },
+                      qualityAsOf: { type: 'string', format: 'date' },
+                      notes: { type: 'string' },
+                    },
+                    required: ['quality', 'qualityAsOf', 'notes'],
+                  },
+                },
+                stressLevel: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      level: { type: 'string' },
+                      levelAsOf: { type: 'string', format: 'date' },
+                      notes: { type: 'string' },
+                    },
+                    required: ['level', 'levelAsOf', 'notes'],
+                  },
+                },
+                socialSupport: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      support: { type: 'string' },
+                      supportAsOf: { type: 'string', format: 'date' },
+                      notes: { type: 'string' },
+                    },
+                    required: ['support', 'supportAsOf', 'notes'],
+                  },
+                },
+                travelHistory: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      location: { type: 'string' },
+                      date: { type: 'string', format: 'date' },
+                      duration: { type: 'string' },
+                      notes: { type: 'string' },
+                    },
+                    required: ['location', 'date', 'duration', 'notes'],
+                  },
+                },
+                familyConditionals: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      condition: { type: 'string' },
+                      typicalCause: { type: 'string' },
+                      typicalAgeOfDiagnosis: { type: 'number' },
+                      notes: { type: 'string' },
+                    },
+                    required: ['condition', 'typicalCause', 'typicalAgeOfDiagnosis', 'notes'],
+                  },
+                },
+                environmentalFactors: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      factor: { type: 'string' },
+                      exposure: { type: 'string' },
+                      exposureAsOf: { type: 'string', format: 'date' },
+                      notes: { type: 'string' },
+                    },
+                    required: ['factor', 'exposure', 'exposureAsOf', 'notes'],
+                  },
+                },
+              },
+
+            },
+            importantNotes: {
+              type: 'array',
+              items: { type: 'string' },
+            },
+          },
+          required: [
+            'gender',
+            'dateOfBirth',
+            'contactInfo',
+            'emergencyContact',
+            'address',
+            'upcomingAppointments',
+            'medicalInformation',
+            'lifeStyleHistory',
+            'importantNotes',
+          ],
+        },
+      },
+      required: [
+        'id',
+        'title',
+        'forenames',
+        'surname',
+        'email',
+        'role',
+        'createdAt',
+        'updatedAt',
+        'data',
+      ],
+
+      medicalHistory: {
+        type: 'array',
+        items: {
+          type: 'object',
+          additionalProperties: true,
+        },
+      },
+    },
+    206: {
+      type: 'object',
+      properties: {
+        title: { type: 'string' },
+        forenames: { type: 'string' },
+        surname: { type: 'string' },
+        email: { type: 'string' },
+        createdAt: { type: 'string', format: 'date-time' },
+        updatedAt: { type: 'string', format: 'date-time' },
+        data: {
+          type: 'object',
+          properties: {
+            gender: { type: 'string' },
+            dateOfBirth: { type: 'string', format: 'date' },
+            contactInfo: {
+              type: 'object',
+              properties: {
+                email: { type: 'string' },
+                phone: { type: 'string' },
+              },
+              required: ['email', 'phone'],
+            },
+            emergencyContact: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  name: { type: 'string' },
+                  phone: { type: 'string' },
+                  relationship: { type: 'string' },
+                },
+                required: ['name', 'phone', 'relationship'],
+              },
+            },
+            address: {
+              type: 'object',
+              properties: {
+                addressLine1: { type: 'string' },
+                addressLine2: { type: 'string' },
+                city: { type: 'string' },
+                county: { type: 'string' },
+                postCode: { type: 'string' },
+              },
+              required: ['addressLine1', 'city', 'county', 'postCode'],
+            },
+            upcomingAppointments: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  appointmentId: { type: 'string' },
+                  date: { type: 'string' },
+                  time: { type: 'string' },
+                  doctorId: { type: 'string' },
+                  doctorName: { type: 'string' },
+                },
+                required: ['appointmentId', 'date', 'time', 'doctorId', 'doctorName'],
+              },
+            },
+            medicalInformation: {
+              type: 'object',
+              properties: {
+                bloodType: { type: 'string' },
+                sexAtBirth: { type: 'string' },
+                conditions: {
+                  type: 'array',
+                  items: { type: 'string' },
+                },
+                allergies: {
+                  type: 'array',
+                  items: { type: 'string' },
+                },
+              },
+              required: ['bloodType', 'sexAtBirth', 'conditions', 'allergies'],
+            },
+            lifeStyleHistory: {
+              type: 'object',
+              description: 'Defined in LifeStyleFactors type',
+            },
+            importantNotes: {
+              type: 'array',
+              items: { type: 'string' },
+            },
+          },
+          required: [
+            'gender',
+            'dateOfBirth',
+            'contactInfo',
+            'emergencyContact',
+            'address',
+            'upcomingAppointments',
+            'medicalInformation',
+            'lifeStyleHistory',
+            'importantNotes',
+          ],
+        },
+      },
+      required: [
+        'title',
+        'forenames',
+        'surname',
+        'email',
+        'createdAt',
+        'updatedAt',
+        'data',
+      ],
+    },
+    400: {
+      type: 'object',
+      properties: {
+        error: { type: 'string' },
+        message: { type: 'string' },
+      },
+      required: ['error', 'message'],
+    },
+    403: {
+      type: 'object',
+      properties: {
+        error: { type: 'string' },
+        message: { type: 'string' },
+      },
+      required: ['error', 'message'],
+    },
+    500: {
+      type: 'object',
+      properties: {
+        error: { type: 'string' },
+        message: { type: 'string' },
+      },
+      required: ['error', 'message'],
+    },
+  },
+};
+
+export default schemaGetPatient;
