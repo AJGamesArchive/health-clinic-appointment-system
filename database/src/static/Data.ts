86d7345035559ebc441bfb07466b05ba848be87b
jess-excell <jessexcell@outlook.com>
2025-05-18 10:59:11 +0100

    Web: Add links to created list views on temporary home page for debugging

--- database/src/static/Data.ts
+++ database/src/static/Data.ts
@@ -58,6 +58,38 @@ const dataCreationFunctions = [
         staffRole: "Head Secretary",
       },
     },
+    {
+      title: "Miss",
+      forenames: "Jane",
+      surname: "Smith",
+      email: "janesmith@hcams.com",
+      password: "$2b$10$a7yfUF5n3MzXpID4zwKcauVtL0vqo9XJyHUBE5eLY4IBsbx2OBVcO",
+      role: "Doctor",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      accountData: {
+        specialty: "Cardiovascular Health",
+        contactInfo: {
+        workEmail: "janesmith@hcams.com",
+        workPhone: "01234567890"
+      },
+      workingHours: [],
+      upcomingAppointments: []
+      } 
+    },
+    {
+      title: "Mr",
+      forenames: "John",
+      surname: "Smith",
+      email: "johnsmith@hcams.com",
+      password: "$2b$10$a7yfUF5n3MzXpID4zwKcauVtL0vqo9XJyHUBE5eLY4IBsbx2OBVcO",
+      role: "Patient",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      accountData: {
+        
+      } 
+    },
   ], { ordered: false }),
 ];
 
