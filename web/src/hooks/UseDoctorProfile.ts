4f2af8c77e4342ad43fbd7453c680a0ce193ef0b
jess-excell <jessexcell@outlook.com>
2025-05-19 09:57:06 +0100

    Web: Fix error not recognising Doctor work schedule updates. Also add template for "edit mode" for Doctor profile.

--- web/src/hooks/UseDoctorProfile.ts
+++ web/src/hooks/UseDoctorProfile.ts
@@ -4,6 +4,8 @@ import { cloneDeep } from "lodash";
 import AccountData from "../types/data/AccountData";
 import DoctorData, { DoctorWorkingHours, DoctorContactInfo } from "../types/data/DoctorData";
 import { useParams } from "react-router-dom";
+import { useAuthContext } from "../contexts/AuthContext";
+import { UseRouteAuthHook } from "./security/UseRouteAuth";
 
 interface UseAdminProfileHook {
     profile: UseProfileHook;
@@ -14,11 +16,14 @@ interface UseAdminProfileHook {
     updateField: (newValue: string, firstField: keyof AccountData, secondField?: keyof DoctorData | null, thirdField?: keyof DoctorContactInfo | null) => void;
     getSchedule: (day: "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday") => DoctorWorkingHours | undefined;
     updateSchedule: (day: "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday", newTime: string, type: "start" | "end") => void;
+    isAdmin: boolean;
 };
 
 export function useDoctorProfile(): UseAdminProfileHook {
     // States
     const { id } = useParams();
+    const auth: UseRouteAuthHook = useAuthContext();
+    const isAdmin = auth.user?.role === "Admin"
     
     const useProfile = id? useAccountProfile(id, "Doctor") : useAccountProfile();
     const [ updateDisabled, setUpdateDisabled ] = useState<boolean>(true);
@@ -36,25 +41,34 @@ export function useDoctorProfile(): UseAdminProfileHook {
     function updateSchedule(day: "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday", newTime: string, type: "start" | "end") {
         if (!modifiedProfile?.doctorData?.workingHours) { return; }
         var toUpdate: DoctorWorkingHours | undefined = modifiedProfile?.doctorData?.workingHours.find((workingDay) => workingDay.day === day);
-        if (!toUpdate || toUpdate === undefined) { return; }
-        if (type === "start") {
-            toUpdate.startTime = newTime;
+        console.log(toUpdate)
+        
+        if (!toUpdate || toUpdate === undefined) { 
+            toUpdate = {
+                day: day,
+                startTime: "",
+                endTime: "",
+            }
+            var newSchedule = [...modifiedProfile.doctorData.workingHours, toUpdate]
         }
         else {
-            toUpdate.endTime = newTime;
-        };
+            var newSchedule = (modifiedProfile.doctorData.workingHours).map((workingDay) => {
+            return (workingDay.day === day) 
+                ? {...toUpdate, 
+                    startTime: type === "start" ? newTime : workingDay.startTime,
+                    endTime: type === "end" ? newTime : workingDay.endTime
+                } as DoctorWorkingHours
+                : workingDay;
+            });
+        }
 
-        var newSchedule = (modifiedProfile.doctorData.workingHours).map((workingDay) => {
-            return workingDay.day === day ? toUpdate! : workingDay;
-        });
-
-        if (newSchedule.length === 0) { return; }
+        if (newSchedule.length === 0) { return; };
         
         setModifiedProfile({...modifiedProfile, doctorData: 
             {...modifiedProfile?.doctorData, workingHours: 
                 newSchedule 
             }
-        })
+        });
         console.log("Should set " + day + " " + type + " time to " + newTime);
     };
 
@@ -78,6 +92,7 @@ export function useDoctorProfile(): UseAdminProfileHook {
 
     return ({
         profile: useProfile,
+        isAdmin,
         updateProfile,
         toggleUpdateEnabled,
         updateDisabled,
